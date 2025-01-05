55fb7c5b89f7c78f11beed05c602d50434282cc7
SH1NY2008 <79332062+SH1NY2008@users.noreply.github.com>
2025-01-05 02:08:52 -0600

    midnight

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['assets.aceternity.com', 'images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        pathname: '/**',  // Use the path pattern to match all image paths
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',  // Use the path pattern to match all image paths
+      },
+    ],
   },
 };
 
--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,19 +1,50 @@
-import { CoverDemo } from "@/components/Cover";
-import { HoverBorderGradientDemo } from "@/components/hover-border-gradient";
-import { Cover } from "@/components/ui/cover";
-import Image from "next/image";
-import { FlipWords } from "@/components/ui/flip-words";
-import { FlipWordsDemo } from "@/components/Flip";
-import { FocusCardsDemo } from "@/components/Focus";
-import { InfiniteMovingCardsDemo } from "@/components/MovingCards";
-import { AnimatedTestimonialsDemo } from "@/components/Testimonials";
-import { TimelineDemo } from "@/components/Timeline";
+'use client';
+import styles from './page.module.css'
+import {
+  viewDay,
+  viewMonthAgenda,
+  viewMonthGrid,
+  viewWeek,
+} from '@schedule-x/calendar'
+import '@schedule-x/theme-default/dist/index.css'
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
+import {createEventModalPlugin} from "@schedule-x/event-modal";
+import {ScheduleXCalendar, useCalendarApp} from "@schedule-x/react";
+import {createEventsServicePlugin} from "@schedule-x/events-service";
+import {useState} from "react";
 
-export default function Page() {
+export default function Home() {
+  const eventsServicePlugin = useState(() => createEventsServicePlugin())[0];
+
+  const calendarApp = useCalendarApp({
+    views: [viewWeek, viewMonthGrid, viewDay, viewMonthAgenda],
+    defaultView: viewWeek.name,
+    callbacks: {
+      onRangeUpdate: (range) => {
+        calendarApp.eventsService.set([
+          {
+            id: '12',
+            title: 'Event 1',
+            start: range.start,
+            end: range.end,
+          },
+        ])
+      }
+    },
+    events: [
+      {
+        id: '12',
+        title: 'Event 1',
+        start: '2023-12-15 06:00',
+        end: '2023-12-15 08:00',
+      },
+    ],
+    selectedDate: '2023-12-15',
+  }, [createDragAndDropPlugin(), createEventModalPlugin(), eventsServicePlugin])
+
   return (
-    
-    <div>
-      
-    </div>
-  );
+    <main className={styles.main}>
+      <ScheduleXCalendar calendarApp={calendarApp} />
+    </main>
+  )
 }
